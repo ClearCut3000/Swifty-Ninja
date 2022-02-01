04320fd0795f7e734724cb68a59bfd1e1a1c973e
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-01 18:43:09 +0300

    Shaping up for action: CGPath and UIBezierPath

--- Swifty Ninja/GameScene.swift
+++ Swifty Ninja/GameScene.swift
@@ -21,6 +21,8 @@ class GameScene: SKScene {
   var lives = 3
   var activeSliceBG: SKShapeNode!
   var activeSliceFG: SKShapeNode!
+  var activeSlicePoints = [CGPoint]()
+  var isSwooshSoundActive = false
 
   //MARK: - Scene
   override func didMove(to view: SKView) {
@@ -68,6 +70,62 @@ class GameScene: SKScene {
     activeSliceFG.lineWidth = 5
     addChild(activeSliceBG)
     addChild(activeSliceFG)
+  }
 
+  func redrawActiveSlice() {
+    if activeSlicePoints.count < 2 {
+      activeSliceBG.path = nil
+      activeSliceFG.path = nil
+      return
+    }
+    if activeSlicePoints.count > 12 {
+      activeSlicePoints.removeFirst(activeSlicePoints.count - 12)
+    }
+
+    let path = UIBezierPath()
+    path.move(to: activeSlicePoints[0])
+    for i in 1 ..< activeSlicePoints.count {
+      path.addLine(to: activeSlicePoints[i])
+    }
+    activeSliceBG.path = path.cgPath
+    activeSliceFG.path = path.cgPath
+  }
+
+  func playSwooshSound() {
+    isSwooshSoundActive = true
+    let randomNumber = Int.random(in: 1...3)
+    let soundName = "swoosh\(randomNumber).caf"
+    let swooshSound = SKAction.playSoundFileNamed(soundName, waitForCompletion: true)
+    run(swooshSound) { [weak self] in
+      self?.isSwooshSoundActive = false
+    }
+  }
+
+  //MARK: - Touch Methods
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let location = touch.location(in: self)
+    activeSlicePoints.append(location)
+    redrawActiveSlice()
+    if !isSwooshSoundActive {
+      playSwooshSound()
+    }
+  }
+
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    activeSliceBG.run(SKAction.fadeOut(withDuration: 0.25))
+    activeSliceFG.run(SKAction.fadeOut(withDuration: 0.25))
+  }
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    activeSlicePoints.removeAll(keepingCapacity: true)
+    let location = touch.location(in: self)
+    activeSlicePoints.append(location)
+    redrawActiveSlice()
+    activeSliceBG.removeAllActions()
+    activeSliceFG.removeAllActions()
+    activeSliceBG.alpha = 1
+    activeSliceFG.alpha = 1
   }
 }
